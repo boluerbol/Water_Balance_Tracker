066df0f8d4f9278b5ccc2f8e9bf80fd935ef4b34
erba <92913666+boluerbol@users.noreply.github.com>
2024-11-04 15:30:53 +0600

    Delete water_balance_tracker directory
    it was an error  when i pushed

--- water_balance_tracker/src/main/java/com/erbaproger/water_balance_tracker/repositories/WaterRecordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.erbaproger.water_balance_tracker.repositories;
-
-import com.erbaproger.water_balance_tracker.entities.WaterRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WaterRecordRepository extends JpaRepository<WaterRecord, Long> { }
-
